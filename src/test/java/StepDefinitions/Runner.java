521de4c215615c910933e78d831823d2bb4b8c34
wolvesfcfan <harpsohal1983@hotmail.com>
2024-05-12 23:34:52 +0100

    Updated Runner Feature Path

--- src/test/java/StepDefinitions/Runner.java
+++ src/test/java/StepDefinitions/Runner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\Win10 VM\\IdeaProjects\\DemoProject\\src\\main\\resources\\Features",
+@CucumberOptions(features="src/main/resources/Features",
         glue = {"StepDefinitions"},
         monochrome = true,
         plugin = {"pretty", "junit:reports/JunitReports/report.xml",
